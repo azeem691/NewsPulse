f6bc3b2439eabf67e2a221d4be1871481f8bf9fe
Abdul Azeem <azeemabdul691@gmail.com>
2025-03-04 22:38:34 +0530

    changed theme background

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,68 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import techImg from '../assets/images/tech.jpg'
-import wordlImg from '../assets/images/world.jpg'
-import sportsImg from '../assets/images/sports.jpg'
-import scienceImg from '../assets/images/science.jpg'
-import healthImg from '../assets/images/health.jpg'
-import entertainmentImg from '../assets/images/entertainment.jpg'
-import nationImg from '../assets/images/nation.jpg'
-import noImg from '../assets/images/no-img.png'
-import './News.css'
-import axios from 'axios'
-import NewsModal from './NewsModal'
+import React, { useEffect, useState } from "react";
+import techImg from "../assets/images/tech.jpg";
+import wordlImg from "../assets/images/world.jpg";
+import sportsImg from "../assets/images/sports.jpg";
+import scienceImg from "../assets/images/science.jpg";
+import healthImg from "../assets/images/health.jpg";
+import entertainmentImg from "../assets/images/entertainment.jpg";
+import nationImg from "../assets/images/nation.jpg";
+import noImg from "../assets/images/no-img.png";
+import "./News.css";
+import axios from "axios";
+import NewsModal from "./NewsModal";
 
 const categories = [
-  'general',
-  'world',
-  'business',
-  'technology',
-  'entertainment',
-  'sports',
-  'science',
-  'health',
-  'nation',
-]
+  "general",
+  "world",
+  "business",
+  "technology",
+  "entertainment",
+  "sports",
+  "science",
+  "health",
+  "nation",
+];
 
 const News = () => {
-  const [headline, setHeadline] = useState(null)
-  const [news, setNews] = useState([])
-  const [selectedCategory, setSelectedCategory] = useState('general')
-  const [showModal, setShowModal] = useState(false)
-  const [selectedArticle, setSelectedArticle] = useState(null)
+  const [headline, setHeadline] = useState(null);
+  const [news, setNews] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("general");
+  const [showModal, setShowModal] = useState(false);
+  const [selectedArticle, setSelectedArticle] = useState(null);
 
   useEffect(() => {
     const fetchNews = async () => {
-      const url = `https://gnews.io/api/v4/top-headlines?category=${selectedCategory}&lang=en&apikey=9c404c552f6102517c9c531e4d8475da`
-      const response = await axios.get(url)
+      const url = `https://gnews.io/api/v4/top-headlines?category=${selectedCategory}&lang=en&apikey=9c404c552f6102517c9c531e4d8475da`;
+      const response = await axios.get(url);
 
-      const fetchedNews = response.data.articles
+      const fetchedNews = response.data.articles;
 
       fetchedNews.forEach((article) => {
         if (!article.image) {
-          article.image = noImg
+          article.image = noImg;
         }
-      })
+      });
 
-      setHeadline(fetchedNews[0])
-      setNews(fetchedNews.slice(1, 7))
+      setHeadline(fetchedNews[0]);
+      setNews(fetchedNews.slice(1, 7));
 
-      console.log(news)
-    }
+      console.log(news);
+    };
 
-    fetchNews()
-  }, [selectedCategory])
+    fetchNews();
+  }, [selectedCategory]);
 
   const handleCategoryClick = (e, category) => {
-    e.preventDefault()
-    setSelectedCategory(category)
-  }
+    e.preventDefault();
+    setSelectedCategory(category);
+  };
 
   const handleArticleClick = (article) => {
-    setSelectedArticle(article)
-    setShowModal(true)
+    setSelectedArticle(article);
+    setShowModal(true);
 
-    console.log(article)
-  }
+    console.log(article);
+  };
 
   return (
     <div className="news-app">
@@ -112,10 +112,9 @@ const News = () => {
         <p className="copyright">
           <span>News App</span>
         </p>
-        <p>&copy; All Rights Reserved. By Code And Create</p>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
